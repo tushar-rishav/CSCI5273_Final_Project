1e8bc69185b3b9a6b68dcd785464b7779517c40d
Tushar Gautam <tushar.rishav@gmail.com>
2022-11-29 18:03:38 -0700

    Define RPC for metadata and node servers

--- src/metadata_server.js
+++ src/metadata_server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const grpc                      = require('@grpc/grpc-js');
 const assert                    = require('assert');
 const config                    = require('config'); 
@@ -5,27 +7,19 @@ const config                    = require('config');
 const {getServer, protoLoader, getLogger}  = require('./util/grpc_util');
 
 const CONFIG_ROOT = 'MetadataService'
-const HOST      = config.get(`${CONFIG_ROOT}.hostConfig.host`);
-const PORT      = config.get(`${CONFIG_ROOT}.hostConfig.port`);
-const logger    = getLogger(config.get(`${CONFIG_ROOT}.name`));
 
-var route_map = { 
-    DiscoverParent: DiscoverParent
-}
+var host      = config.get(`${CONFIG_ROOT}.hostConfig.host`);
+var port      = config.get(`${CONFIG_ROOT}.hostConfig.port`);
+var logger    = getLogger(config.get(`${CONFIG_ROOT}.name`));
 
-function startServer() {
-    let proto_descriptor = protoLoader(config.get(`${CONFIG_ROOT}.protobuf_file`));
-    let routeServer = getServer(proto_descriptor.metadata.MetadataService, route_map);
+var proto_descriptor    = protoLoader(config.get(`${CONFIG_ROOT}.protobuf_file`));
+var proto_service       = proto_descriptor.metadata.MetadataService;
 
-    routeServer.bindAsync(`${HOST}:${PORT}`,
-                            grpc.ServerCredentials.createInsecure(), (err, port) => {
-                                assert.ifError(err);
-                                routeServer.start();
-                            }
-                        );
-}
-
-function DiscoverParent(call, callback) {
+function join_cluster(call, callback) {
+    /**
+     * Receives node join request and returns suitable parent_node based on
+     * the stored whereabouts metadata.
+     */
     var response = {
         parent: {
             host: {
@@ -34,9 +28,28 @@ function DiscoverParent(call, callback) {
         }
     };
 
-    logger.debug("DiscoverParent called");
+    logger.debug("JoinCluster called");
     
     callback(null, response);
+}
+
+function publish_whereabouts(call, callback) {
+    /**
+     * Periodically receive and persist node's whereabouts 
+     */
+
+    callback(null, null);
+}
+
+var route_map = { 
+    JoinCluster: join_cluster,
+    PublishWhereabouts: publish_whereabouts
+}
+
+function startServer() {
+    getServer(host, port, proto_service, route_map, (err, resp) => {
+        assert.ifError(err);
+    });
 }
 
 startServer();
